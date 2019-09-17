514b30338fca59cf4b44e1014d4c79f2c8b0fa1e
Andrew Holzer <andrew.holzer@digilent.com>
2019-09-17 02:27:39 +0100

    Update example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,29 +1,69 @@
-#include "../src/dlog-utils.h"
 #include <iostream>
-#include <chrono>
+#include <fstream>
 
-using namespace DlogUtil;
+#include "dlog.h"
+#include "kaitai/kaitaistream.h"
 
-int main(int argc, const char * argv[])
-{
-    if (argc != 3) { return 1; }
-    DlogConvert myConvert;
-    std::string dlogPath = argv[1];
-    std::string outputName = argv[2];
-    DEBUG("\nPath to dlog: ");
-    DEBUGLN(dlogPath);
-    DEBUG("Output file: ");
-    DEBUGLN(outputName);
+#ifndef DOUBLE_PRECISION 
+    #define DOUBLE_PRECISION 5 // Made into a preprocessing directive to be set from compiler flags
+#endif
 
-    #ifdef DEBUGPRINT
-        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-    #endif
-    myConvert.ConvertFile(dlogPath, outputName);
-    #ifdef DEBUGPRINT
-        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-        DEBUG(std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count());
-        DEBUGLN(" microseconds");
-    #endif
+int main(int argc, char**argv) {
+    if (argc != 3)
+        return 1;
+
+    std::string dlogPath = argv[1], outputName = argv[2], outputFileName = outputName + ".csv";
+    std::ifstream infile(dlogPath, std::istream::binary);
+    kaitai::kstream kstream(&infile);
+    dlog_t log(&kstream);
+    std::ofstream outfile(outputFileName);
+
+    auto header = log.header();
+    auto data = log.data();
+    auto dt = 1.0 / header->sample_rate();
+
+    std::string stopReason;
+    switch(header->stop_reason()) {
+        case dlog_t::stop_reasons_t::STOP_REASONS_NORMAL:
+            stopReason = "Log Completed Normally\n";
+            break;
+        case dlog_t::stop_reasons_t::STOP_REASONS_FORCED:
+            stopReason = "Log Forced\n";
+            break;
+        case dlog_t::stop_reasons_t::STOP_REASONS_ERROR:
+            stopReason = "Log Error\n";
+            break;
+        case dlog_t::stop_reasons_t::STOP_REASONS_OVERFLOW:
+            stopReason = "Log Overflow\n";
+            break;
+        case dlog_t::stop_reasons_t::STOP_REASONS_UNKNOWN:
+        default:
+            stopReason = "Log Unknown Error\n";
+            break;
+    }
+    
+    outfile << stopReason;
+    outfile.precision(DOUBLE_PRECISION);
+
+    int cnt = 0;
+    auto initialIndex = header->start_index();
+    for (auto& sample : *data->samples()) {
+        outfile << std::fixed << dt * (initialIndex + cnt) << ", ";
+        
+        cnt++;
+        
+        for (auto& channel : *sample->channel()) {
+			outfile << 1.0 * channel / header->voltage_scale();
+
+            if (&channel != &sample->channel()->back())
+                outfile << ", ";
+        }
+
+        outfile << "\n";
+    }
+
+    outfile.close();
+    infile.close();
 
     return 0;
 }
